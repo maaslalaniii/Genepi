77b72580543df0af56b592c5480d03e1f7a1f270
Maas Lalani <maas@live.ca>
2017-03-25 23:45:44 -0400

    Implemented display of the algorithm

--- sketch.js
+++ sketch.js
@@ -1,10 +1,27 @@
-let population
-
 function setup() {
+
+  // setup the dom elements to show progress
+  display = createP('PI')
+  result = createP('Evolving...')
+
   // create a population with random genetics
   population = new Population()
+  
 }
 
 function draw() {
+
+  // calculate the fitness of the population
+  population.calculateFitness()
+
+  // show the most fit number of the population
+  display.html(population.strongest())
+
+  // check to see if the algorithm was successful
+  if (population.update() == pi.join('')) {
+    result.html('Evolution completed!')
+  } else {
+    population.evolve()
+  }
 
 }
