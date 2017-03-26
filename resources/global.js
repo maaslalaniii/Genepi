6806f8f8e20d9874b3f7f3576d2e03abe9b8f754
Maas Lalani <maas@live.ca>
2017-03-25 23:41:04 -0400

    Configured global variables

---
+++ resources/global.js
@@ -0,0 +1,17 @@
+// number of elements in the population
+let N = 100
+
+// pi - the target to evolve into used only in the fitness function
+let pi = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9]
+
+// the number of digits to evolve
+let digits = pi.length
+
+// the population of numbers
+let population
+
+// the display of the most fit number
+let display
+
+// whether the algorithm is progressing or has completed
+let result
