83d2d8fe5f1fdaf4a99126e3cd7db95ca1c6c96e
Maas Lalani <maas@live.ca>
2017-03-26 23:57:18 -0400

    Added . to the PI representation

--- sketch.js
+++ sketch.js
@@ -19,19 +19,21 @@ function draw() {
   population.calculateFitness()
 
   // show the most fit number of the population
-  display.html(population.strongest())
+  guess = population.strongest().substr(0, 1) + "." + population.strongest().substr(1)
+  display.html(guess)
 
   // check to see if the algorithm was successful
   if (population.strongest() == pi.join('')) {
     result.html('Evolution completed!')
   } else {
     population.evolve()
+
     
     console.log(population)
 
     generation.innerHTML = population.generation
     
-    createP(population.strongest()).parent(cache)
+    createP(guess).parent(cache)
     createP(population.averageFitness()).parent(fitness)
     
     cache.scrollTop = cache.scrollHeight
